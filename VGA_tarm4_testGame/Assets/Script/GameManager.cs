32bc76dfec4167c35ac32420981816ca2f680f60
hujishima takumi <hujishima.t425@gmail.com>
2019-11-15 23:16:37 +0900

    create GameManagerscript

---
+++ VGA_tarm4_testGame/Assets/Script/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    Gamestatus m_gameStatus = Gamestatus.NonInitialized;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch(m_gameStatus)
+        {
+            case Gamestatus.NonInitialized:
+                break;
+
+            case Gamestatus.Initialized:
+                break;
+
+            case Gamestatus.Ingame:
+                break;
+
+            case Gamestatus.Failed:
+                break;
+        }
+
+    }
+    enum Gamestatus
+    {
+        /// <summary>/// ゲーム初期化前/// </summary>
+        NonInitialized,
+        /// <summary>/// ゲーム初期化済み、開始前/// </summary>
+        Initialized,
+        /// <summary>/// ゲーム進行中/// </summary>
+        Ingame,
+        /// <summary>/// 失敗時処理を実行/// </summary>
+        Failed,
+    }
+}
