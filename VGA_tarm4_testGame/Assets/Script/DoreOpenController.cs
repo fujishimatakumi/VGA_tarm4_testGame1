636615da1e112df130675af0a2cdbdc8a43762ce
hujishima takumi <hujishima.t425@gmail.com>
2019-11-20 23:17:01 +0900

    create DoreOpenController

---
+++ VGA_tarm4_testGame/Assets/Script/DoreOpenController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoreOpenController : MonoBehaviour
+{
+    //float m_height;
+    float m_width;
+    //float m_positionX;
+    float m_positionY;
+    [SerializeField] bool m_judgment = false;
+    [SerializeField] float m_openSpead = 0.1f;
+    GameObject ga;
+    float m_downDistans;
+    Vector3 m_nowPosition;
+    // Start is called before the first frame update
+    void Start()
+    {   
+        m_width = this.gameObject.transform.lossyScale.y;
+        m_positionY = this.gameObject.transform.position.y;
+        ga = this.gameObject;
+        m_downDistans = m_positionY - m_width;
+        m_nowPosition = this.gameObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   
+        if (m_judgment)
+        {
+            if (m_nowPosition.y > m_downDistans)
+            {
+                ga.gameObject.transform.Translate(0f, m_openSpead, 0f);
+            }
+            
+        }
+       
+    }
+}
